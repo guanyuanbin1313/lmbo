4628cb7b0d54f6e0902652ad09b2e7955cd69ecf
zcq1933 <1933748107@qq.com>
2019-11-26 14:20:10 +0800

    上传前端

--- Reader-font-end-raw/reader/src/App.js
+++ Reader-font-end-raw/reader/src/App.js
@@ -9,31 +9,52 @@ import Register from './components/register';
 
 const {Search} = Input;
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="container">
-          <div className="header">
-            <a href="/"><span className="logo-title">Reader</span></a>
-            <Search
-            className="search-input"
-            placeholder="搜索你要的书"
-            enterButton="搜索"
-            size="large"
-            onSearch={value => console.log(value)}
-            />
-              <div className="login_register">
-                <Link style={{color: 'white'}} to="/login">登录</Link>
-                <span style={{color: 'grey',margin:'0 1vw 0 1vw'}}>|</span>
-                <Link style={{color: 'white'}} to="/register">注册</Link>
-              </div>
-          </div>
-      </div>
-      <Route path="/register" component={Register}/>
-      <Route path="/login" component={Login}/>
-    </BrowserRouter>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponse:""
+    };
+  }
 
-  );
+  callApi() {
+    fetch("http://localhost:9000/testApi",{
+      method:'GET',
+      mode : 'cors'
+    })
+    .then(res=> res.text())
+    .then(res => this.setState({apiResponse:res}))
+    .catch(err => err);
+  }
+  componentDidMount() {
+    this.callApi();
+    console.log(this.state.apiResponse);
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+        <div className="header">
+        <a href="/"><span className="logo-title">Reader</span></a>
+        <Search
+        className="search-input"
+        placeholder="搜索你要的书"
+        enterButton="搜索"
+        size="large"
+        onSearch={value => console.log(value)}
+        />
+        <div className="login_register">
+        <Link style={{color: 'white'}} to="/login">登录</Link>
+        <span style={{color: 'grey',margin:'0 1vw 0 1vw'}}>|</span>
+        <Link style={{color: 'white'}} to="/register">注册</Link>
+        </div>
+        </div>
+        </div>
+        <Route path="/register" component={Register}/>
+        <Route path="/login" component={Login}/>
+      </BrowserRouter>
+
+    );
+
+  }
 }
-
-export default App;
