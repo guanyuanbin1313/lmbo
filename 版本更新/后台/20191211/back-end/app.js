5b388826fda9bfb4ed8e48ab2e76ecefd68c1cde
官源斌 <33643072+guanyuanbin1313@users.noreply.github.com>
2019-12-11 14:06:16 +0800

    123

--- 版本更新/后台/20191211/back-end/app.js
+++ 版本更新/后台/20191211/back-end/app.js
@@ -25,7 +25,7 @@ app.all('*', function (req, res, next) {
   //允许请求资源的方式
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By", ' 3.2.1');
-  res.header("Cache-Control", "no-store")
+  res.header("Cache-Control", "no-store");
   next();
 });
 
