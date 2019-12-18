0642fc63e7aeca333995c53c0558d07d7ff70058
haddis69 <1474154683@qq.com>
2019-12-18 16:58:12 +0800

    按钮美化

--- 版本更新/前台/reader/20191210及之前/LinkSecret.js
+++ 版本更新/前台/reader/20191210及之前/LinkSecret.js
@@ -6,7 +6,7 @@ export default function LinkSecret() {
         <form action="#" method="post" style={{margin:"0 auto",borderRadius:'2vw',marginTop:'10vw',width:'45%',height:'25vw',backgroundColor:'rgb(204,204,204)'}}>
             <input name="shareLink" type="password" placeholder=" 请输入链接密码：" style={{outline:'none',borderRadius:'1vw',marginLeft:"2vw",marginTop:'10vw',width:'40vw',height:"4vw"}}>
             </input>
-            <input style={{marginTop:'1vw',width:'30vw',borderRadius:'1vw',height:'4vw',marginLeft:"6vw",}} type="submit"></input>
+            <input style={{marginTop:'1vw',width:'25vw',borderRadius:'2vw',height:'4vw',marginLeft:"6vw",}} type="submit"></input>
         </form>
     </div>
   )
