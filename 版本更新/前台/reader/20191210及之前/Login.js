8e6ab6b1dabb20d3ad351a97e6de9e8d2a207728
haddis69 <1474154683@qq.com>
2019-12-18 16:29:44 +0800

    登录界面改变效果

--- 版本更新/前台/reader/20191210及之前/Login.js
+++ 版本更新/前台/reader/20191210及之前/Login.js
@@ -6,7 +6,7 @@ export default function Login() {
       <form action="/login" method="post">
         <Input placeholder="请输入用户名" type="primary" name="username"></Input>
         <Input style={{margin:"1vh 0 1vh 0"}} placeholder="请输入密码" name="password" type="primary"></Input>
-        <button style={{height:'3vh',width:'40%',border:'none'}} type="submit">登录</button>
+        <button style={{height:'4vh',width:'40%',border:'none'}} type="submit">登录</button>
       </form>
     </div>
   )
